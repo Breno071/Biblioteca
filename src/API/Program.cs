6fd541a335e80f7e8e1f9930d8fb7b802dbc6af6
Breno Narde Silva <brenonarde@gmail.com>
2024-01-21 19:48:19 -0300

    Inject serilog configuration on program.cs

--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,9 +1,26 @@
+using Serilog.Sinks.MSSqlServer;
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Inject logging
+builder.Services.AddLogging(options =>
+{
+    var logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.MSSqlServer(
+                    connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+                    sinkOptions: new MSSqlServerSinkOptions { TableName = "TB_LOG", AutoCreateSqlTable = true }
+                )
+                .CreateLogger();
+
+    options.AddSerilog(logger);
+});
 
 var app = builder.Build();
 
