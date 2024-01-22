62b940cf631ace7335973a14fbee938d68384939
Breno Narde Silva <brenonarde@gmail.com>
2024-01-22 16:28:27 -0300

    New Model Reservation

---
+++ src/Domain/Models/Entities/Reservation.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Events;
+using System.ComponentModel.DataAnnotations;
+
+#nullable disable
+namespace Domain.Models.Entities
+{
+    public class Reservation
+    {
+        public event EventHandler<ReservationCreatedEventArgs> ReservationCreated;
+
+        [Key]
+        public Guid Code { get; set; } = Guid.NewGuid();
+        [Required]
+        public Client Client { get; set; }
+        [Required]
+        public ICollection<Book> Books { get; set; }
+        [Required]
+        public DateTime ReservationDate { get; set; }
+        [Required]
+        public DateTime ReturnDate { get; set; }
+        public bool IsReturned { get; set; } = false;
+
+        public virtual void OnReservationCreated()
+            => ReservationCreated?.Invoke(this, new ReservationCreatedEventArgs(this));
+    }
+}
