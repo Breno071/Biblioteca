fba8f2246ddea926f3fa6fea654f52e22482a89d
Breno Narde Silva <brenonarde@gmail.com>
2024-01-22 16:28:43 -0300

    New Table Reservations on BaseDbContext

--- src/Infraestructure/Data/BaseDbContext.cs
+++ src/Infraestructure/Data/BaseDbContext.cs
@@ -7,5 +7,6 @@ namespace Infraestructure.Data
     {
         public DbSet<Book> Books { get; set; }
         public DbSet<Client> Clients { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
     }
 }
