a57008ab4aa6a0e00eda6dd67638d3ad1c641dd7
Breno Narde Silva <brenonarde@gmail.com>
2024-01-23 15:10:34 -0300

    Found library make tests inside container

---
+++ tests/Tests/BaseIntegrationTest.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Domain.Interfaces;
+using DotNet.Testcontainers.Builders;
+using Infraestructure.Configuration;
+using Infraestructure.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.MsSql;
+
+namespace Tests
+{
+    public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestWebApiFactory>, IDisposable
+    { 
+        protected readonly IServiceScope _scope;
+        protected readonly IMapper _mapper;
+        protected readonly IReservationService _reservationService;
+        protected readonly BaseDbContext DbContext;
+
+        public BaseIntegrationTest(IntegrationTestWebApiFactory factory) 
+        {
+            _scope = factory.Services.CreateScope();
+            _mapper = _scope.ServiceProvider.GetRequiredService<IMapper>();
+            _reservationService =_scope.ServiceProvider.GetRequiredService<IReservationService>();
+            DbContext = _scope.ServiceProvider.GetRequiredService<BaseDbContext>();
+        }
+
+        public void Dispose()
+        {
+            _scope?.Dispose();
+            DbContext?.Dispose();
+        }
+    }
+}
---
+++ tests/Tests/IntegrationTestWebApiFactory.cs
@@ -0,0 +1,55 @@
+﻿using Docker.DotNet.Models;
+using DotNet.Testcontainers.Builders;
+using DotNet.Testcontainers.Images;
+using Infraestructure.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.MsSql;
+using Testcontainers.PostgreSql;
+
+namespace Tests
+{
+    public class IntegrationTestWebApiFactory : WebApplicationFactory<Program>, IAsyncLifetime
+    {
+        private readonly MsSqlContainer _msSqlContainer = new MsSqlBuilder()
+                            .WithImage("mcr.microsoft.com/mssql/server:2019-latest")
+                            .WithWaitStrategy(
+                                Wait.ForUnixContainer()
+                                    .UntilPortIsAvailable(1433))
+                        .Build();
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureTestServices(services =>
+            {
+                var descriptorType =
+                    typeof(DbContextOptions<BaseDbContext>);
+
+                var descriptor = services
+                    .SingleOrDefault(s => s.ServiceType == descriptorType);
+
+                if (descriptor is not null)
+                {
+                    services.Remove(descriptor);
+                }
+
+                services.AddDbContext<BaseDbContext>(options =>
+                    options.UseSqlServer(_msSqlContainer.GetConnectionString()));
+            });
+        }
+
+        public Task InitializeAsync()
+        {
+            return _msSqlContainer.StartAsync();
+        }
+
+        Task IAsyncLifetime.DisposeAsync()
+        {
+            return _msSqlContainer.StopAsync();
+        }
+    }
+}
